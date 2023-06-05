bed3bf31b3e8cbeb34aee9878f3600bcf342e10d
malinpalo <malinpalo.mp@gmail.com>
2023-06-05 20:42:37 +0000

    Add allowed_hosts to solve error

--- gro_drf/settings.py
+++ gro_drf/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-!1wzb+2yc&!+pxv=6%-^%vi$o#o6(bcf-v!wx$zglr&%s7cyu)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
