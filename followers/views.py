ffdfd6878df84dd28a5fa925da568fd52f0db64f
malinpalo <malinpalo.mp@gmail.com>
2023-06-08 19:13:45 +0000

    Add followerlist and followerdetail views

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from gro_drf.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    Lists all followers of/for a user, 
+    follow a user if logged in,
+    assoociate the current logged in usr with a follower.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrive or unfollow a follower if owner.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
