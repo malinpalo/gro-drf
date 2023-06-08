b15c7702926d6348bd597695ca90c923ff3cea90
malinpalo <malinpalo.mp@gmail.com>
2023-06-08 21:08:06 +0000

    Add count fields for profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from gro_drf.permissions import IsOwnerOrReadOnly
@@ -8,8 +9,22 @@ class ProfileList(generics.ListAPIView):
     """
     Lists all profiles
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-date_created')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__date_created',
+        'owner__followed__date_created',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -17,5 +32,9 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrive or update your profile if owner.
     """
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-date_created')
     permission_classes = [IsOwnerOrReadOnly]
