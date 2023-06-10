cf36f203e35c1c55a5ccdc9bfa91205256cbb0b1
malinpalo <malinpalo.mp@gmail.com>
2023-06-10 07:23:21 +0000

    Add contact model and serializer

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'{self.name} - {self.last_name}'
---
+++ contact/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = [
+            'first_name', 'last_name', 'email',
+            'subject', 'message',
+        ]
