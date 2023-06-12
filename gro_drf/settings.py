eb8da9c2be5ebaa1fc97bee43be354744e5f18b4
malinpalo <malinpalo.mp@gmail.com>
2023-06-12 09:41:59 +0000

    Install django allauth

--- gro_drf/settings.py
+++ gro_drf/settings.py
@@ -51,8 +51,13 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
-    'rest_framework_authtoken',
+    'rest_framework.authtoken',
     'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 
     'profiles',
     'posts',
@@ -61,7 +66,7 @@ INSTALLED_APPS = [
     'followers',
     'contact',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
