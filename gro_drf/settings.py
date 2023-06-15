14a3d9ffe0df7bde8e0a7f94a91431cb3e250e5d
malinpalo <malinpalo.mp@gmail.com>
2023-06-15 18:20:05 +0000

    Update client origin to solve error

--- gro_drf/settings.py
+++ gro_drf/settings.py
@@ -110,11 +110,10 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^([^.]+)', os.environ.get(
-        'CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV',
+    ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
