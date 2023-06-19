03e38062ab64db88f461e7666135b44c4027db5f
malinpalo <malinpalo.mp@gmail.com>
2023-06-19 16:16:17 +0000

    Modify settings to solve an deployment error

--- gro_drf/settings.py
+++ gro_drf/settings.py
@@ -110,11 +110,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+     ]
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-malinpalo-grodrf-5s3w3ee5g7o.ws-eu100.gitpod.io', 'https://gro-drf.herokuapp.com']
 
 CORS_ALLOW_CREDENTIALS = True
 
